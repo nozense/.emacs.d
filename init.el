47128263c049ea18ee976ab48afbe69ca4ff0367
nozense <kbm.wiberg@pm.me>
2023-12-01 15:56:54 +0100

    Moved to custom.el and gitignored it.

--- init.el
+++ init.el
@@ -16,6 +16,10 @@
  user-full-name "NoZenSe"
  user-mail-address "nonsens@nozen.se")
 
+;; Move the set-custom to a separate file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Some random settings ;;
@@ -120,16 +124,3 @@
 ;;;;;;;;;;;;;
 ;;;; END ;;;;
 ;;;;;;;;;;;;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(php-mode markdown-mode htmlize avy dracula-theme company vertico which-key no-littering)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
