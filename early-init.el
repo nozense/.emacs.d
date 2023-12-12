7c4495e0e61549d327120cea58d72d6a6e173ada
nozense <kbm.wiberg@pm.me>
2023-12-12 09:03:33 +0100

    Trying to fix an error - still eln-chache

--- early-init.el
+++ early-init.el
@@ -4,11 +4,3 @@
  (unless (file-exists-p myTmpDir)
     (make-directory myTmpDir t))
 
-
-;; Put eln-cache dir in cache directory
-;; NOTE the method for setting the eln-cache dir depends on the emacs version
-;; https://github.com/SystemCrafters/crafted-emacs/issues/103
-(when (fboundp 'startup-redirect-eln-cache)
-  (if (< (string-to-number emacs-version) 29)
-      (add-to-list 'native-comp-eln-load-path (concat myTmpDir "eln-cache/"))
-    (startup-redirect-eln-cache (concat myTmpDir "eln-cache/"))))
