1e8683ea5940e3108b054bff9c3bea27b7c77c72
nozense <kbm.wiberg@pm.me>
2023-12-17 14:44:21 +0100

    Forgotten to push something?

--- elisp/nozenlapz.el
+++ elisp/nozenlapz.el
@@ -9,6 +9,19 @@
  (add-to-list 'custom-theme-load-path "~/org/draculaPRO/themes/emacs/")
    (load-theme 'dracula-pro-blade t)
 
+;; Pyhton
+;; pip install python-language-server[all]
+
+
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (
+         (python-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
 
 ;;;;;;;;;;;;;;;;;;;
 ;;               ;;
