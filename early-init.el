594f4ae7bd602d4595a995af8f47766b4a853147
nozense <kbm.wiberg@pm.me>
2023-12-21 14:55:29 +0100

    more cleaning

--- early-init.el
+++
@@ -1,6 +0,0 @@
-
-;;Make a directory for temp/local files - Mostly for no-littering!
- (setq myTmpDir (concat "~/" ".tmpEmacsFiles/"))
- (unless (file-exists-p myTmpDir)
-    (make-directory myTmpDir t))
-
